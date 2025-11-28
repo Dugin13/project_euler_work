a170fc40117e29cfd8dff3cf55db3dacebf43739
Dugin13 <niels@lichking.dk>
2025-11-28 08:27:33 +0100

    problem 48

---
+++ projecteuler048/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        BigInteger sum = BigInteger.Zero;
+        int target = 1000;
+        int digitLimit = 10;
+        for (int i = 1; i <= target; i++)
+        {
+            sum += BigInteger.Pow(i, i);
+            sum %= BigInteger.Pow(10, digitLimit);
+        }
+        string result = sum.ToString().Substring(0, digitLimit);
+        Console.WriteLine(result);
+    }
+}

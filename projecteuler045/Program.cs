d39577283dadad572489c86c238977cf15df1184
Dugin13 <niels@lichking.dk>
2025-11-28 07:56:21 +0100

    problem 45

---
+++ projecteuler045/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool found = false;
+        int t=285, p=165, h=143;
+        t++;
+        while (!found)
+        {
+            BigInteger triangleNumber = calTriangleNumber(t);
+            BigInteger pentagonalNumber = calPentagonalNumber(p);
+            BigInteger hexagonalNumber = calHexagonalNumber(h);
+
+            if (triangleNumber == pentagonalNumber && pentagonalNumber == hexagonalNumber)
+            {
+                Console.WriteLine($"Found number: {triangleNumber} at T={t}, P={p}, H={h}");
+                found = true;
+            }
+            else
+            {
+                if (triangleNumber <= pentagonalNumber && triangleNumber <= hexagonalNumber)
+                {
+                    t++;
+                }
+                else if (pentagonalNumber <= triangleNumber && pentagonalNumber <= hexagonalNumber)
+                {
+                    p++;
+                }
+                else
+                {
+                    h++;
+                }
+            }
+        }
+    }
+
+    private static BigInteger calTriangleNumber(int n)
+    {
+        return (BigInteger)n * (n + 1) / 2;
+    }
+    
+    private static BigInteger calPentagonalNumber(int n)
+    {
+        return (BigInteger)n * (3 * n - 1) / 2;
+    }
+
+    private static BigInteger calHexagonalNumber(int n)
+    {
+        return (BigInteger)n * (2 * n - 1);
+    }
+}
---
+++ projecteuler045/README.md
@@ -0,0 +1,22 @@
+# Project Euler 045
+
+This is a C# console application for solving Project Euler problem 45.
+
+## Getting Started
+
+This project is intended to be a simple C# console app. You can build and run it using the .NET CLI or Visual Studio Code.
+
+## Requirements
+- .NET SDK (6.0 or later recommended)
+
+## Build and Run
+1. Open a terminal in the project root.
+2. Run `dotnet build .` to build the project.
+3. Run `dotnet run .` to execute the program.
+
+## Customization
+Add your solution code to `Program.cs`.
+
+---
+
+For more information on Project Euler problems, visit [Project Euler](https://projecteuler.net/problem=45).

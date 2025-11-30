25b70a9cbd8780a4df66ff775e7565a7a9767fe9
Dugin13 <niels@lichking.dk>
2025-11-30 01:30:59 +0100

    solved problem 56

---
+++ projecteuler056/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    // Project Euler Problem 56 Task:
+    // "Consider natural numbers of the form a^b, where a, b < 100. What is the maximum digital sum?"
+    private static void Main(string[] args)
+    {
+        int limit = 100;
+        int maxSum = 0;
+        int a = 0;
+        int b = 0;
+        for (int i = 1; i < limit; i++)
+        {
+            for (int x = 1; x < limit; x++)
+            {
+                var bigNum = BigInteger.Pow(i, x);
+                int digitSum = bigNum.ToString().Select(c => c - '0').Sum();
+                if (digitSum > maxSum)
+                {
+                    maxSum = digitSum;
+                    a = i;
+                    b = x;
+                }
+            }
+        }
+        Console.WriteLine($"Maximum digital sum: {maxSum} for a = {a} and b = {b}");
+    }
+}
---
+++ projecteuler056/README.md
@@ -0,0 +1,20 @@
+# projecteuler056
+
+This is a C# console application scaffolded for solving Project Euler problem 56.
+
+## How to Run
+
+1. Make sure you have the .NET SDK installed.
+2. Open a terminal in this directory.
+3. Run the following command:
+   
+   ```powershell
+   dotnet run
+   ```
+
+## Project Structure
+- `Program.cs`: Main entry point for the application.
+- `projecteuler056.csproj`: Project configuration file.
+
+## Customization
+Add your solution code for Project Euler problems in `Program.cs` or additional files as needed.
